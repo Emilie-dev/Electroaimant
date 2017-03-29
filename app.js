84d992ba3134cdf055c964e041485c1ab433e94e
Chauvin <chauvin-e@orange.fr>
2017-03-29 12:32:50 +0200

    add function ready

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-for (var i = 0; i <= 50; i++) {
+$(document).ready(function() {
+
+for (var i = 0; i <= 100; i++) {
 	$('<div>').addClass('debris').appendTo('#jeu');
-	$('#jeu').append('<div class="debris">');
+	$('#jeu').append('<div class="debris">').css();
 }
 
 function pointsAléatoires(min, max) {
 	return Math.random() * (max - min) + min;
+	console.log(pointsAléatoires(0, 100));
 }
 
+
+
+
+
+
+
+
+
+
+});
